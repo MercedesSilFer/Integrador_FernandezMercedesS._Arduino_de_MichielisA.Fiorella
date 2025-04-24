8c0b14198fe077f0ac2230a438c8eb6bab3a654b
MercedesFernandez <mercedessilvanafernandez37@gmail.com>
2025-04-24 14:02:04 -0300

    eventos responsive

--- app/Views/front-end/eventos.php
+++ app/Views/front-end/eventos.php
@@ -1,28 +1,28 @@
 <section>
 <div class="container text-center">
     <h1>Tendencias & Noticias</h1>
-  <div class="row align-items-center mt-5 mb-5 pt-3 pb-3">
-    <div class="col">
+  <div class="row align-items-center mt-5 mb-5 pt-3 pb-3 justify-content-evenly">
+    <div class="col-12 col-lg-3 col-md-12">
         <h2>Corrientes diseña Edición 2019</h2>
     <p>Este sábado 30-3 en las Escalinatas del Teatro Vera mega desfile con grandes diseñadores locales. También vas a poder disfrutar de bandas en vivo, moda y diseño.</p>
     </div>
-    <div class="col">
-    <img src="<?php echo base_url('assets/img/corrientesdisena.jpg');?>" alt="" style="width:100%">
+    <div class="col-12 col-lg-3 col-md-7">
+    <img src="<?php echo base_url('assets/img/corrientesdisena.jpg');?>" alt="" style="width:100%" class="img-fluid">
     </div>
-    <div class="col">
+    <div class="col-12 col-lg-3 col-md-5 mt-2">
       <p>Kurundú va a estar presente junto a estos excelentes diseñadores.</p>
     </div>
 
 </div>
-  <div class="row align-items-center mt-5 mb-5 pt-3 pb-3">
-  <div class="col">
+  <div class="row align-items-center mt-5 mb-5 pt-3 pb-3 justify-content-evenly" >
+  <div class="col-12 col-lg-3 col-md-12">
     <h2>Feria Puro Diseño Edición 2018</h2> 
     <p>La Feria Puro Diseño es un evento que reúne a los mejores diseñadores y marcas de Argentina, ofreciendo una amplia variedad de productos de diseño contemporáneo. En esta edición, podrás encontrar desde moda hasta decoración, todo con un enfoque en la calidad y la creatividad.</p>
     </div>
-    <div class="col">
-    <img src="<?php echo base_url('assets/img/feria2018.jpg');?>" alt="" style="width:100%">
+    <div class="col-12 col-lg-3 col-md-7 ">
+    <img src="<?php echo base_url('assets/img/feria2018.jpg');?>" alt="" style="width:100%" class="img-fluid">
     </div>
-    <div class="col">
+    <div class="col col-12 col-lg-3 col-md-5 mt-2">
       <p>Queremos contarles con gran alegría que vamos a participar junto a otros expositores Correntinos de la Feria Puro Diseño 2018. Estamos muy contentos por esta gran oportunidad de poder mostrar al país nuestros productos.</p>
     </div>
     
